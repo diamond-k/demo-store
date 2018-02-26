797e2c65cfc57ae7248d2207bfb1898a3092c30a
XxxDiamondxxX <mzladylush@googlemail.com>
2018-02-26 01:48:22 +0000

    test

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-//export const API_BASE = 'https://eds-online-store.herokuapp.com/api/v1'
-export const API_BASE = 'http://localhost:3000/api/v1'
+export const API_BASE = 'https://eds-online-store.herokuapp.com/api/v1'
+//export const API_BASE = 'http://localhost:3000/api/v1'
